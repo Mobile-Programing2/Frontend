78fff4e71da0b988d207d6d44c2afb091477d83b
“yoonseul0630” <y200630@naver.com>
2024-09-25 15:44:20 +0900

    수정완료

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveㅇㅇ to reload.
+          Edit <code>src/App.js</code> and saveㅇㅇ to reload.tnrh수고요
         </p>
         <a
           className="App-link"
